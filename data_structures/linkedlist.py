91234046e311f3ab464b818589de5b050b83f090
rawrrawrpurpledinosaur <191292L@student.hci.edu.sg>
2024-08-18 17:35:10 +0800

    added delete method

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -6,7 +6,15 @@ class Node:
 class LinkedList: 
     def __init__(self): 
         self.head = None
+        self.size = self.get_size()
 
+    def get_size(self): 
+        probe = self.head
+        count = 0
+        while probe: 
+            count += 1
+            probe = probe.next
+        return count
 
     def append(self, data):
         if self.head == None:
@@ -23,6 +31,13 @@ class LinkedList:
             probe = probe.next
         probe.next = Node(data, probe.next)
 
+    def delete(self, index): 
+        probe = self.head
+        for i in range(index-1): 
+            probe = probe.next
+        probe.next = probe.next.next
+
+
     def __str__(self): 
         probe = self.head
         result = ""
@@ -35,5 +50,6 @@ ll = LinkedList()
 ll.append(1)
 ll.append(2)
 ll.insert(3, 1)
+ll.delete(1)
 print(ll)
 
