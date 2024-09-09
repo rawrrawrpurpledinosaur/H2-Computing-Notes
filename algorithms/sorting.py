0e586983fe7ac36a1ad866603c3d308a1fafcb94
rawrrawrpurpledinosaur <191292l@student.hci.edu.sg>
2024-09-09 20:48:55 +0800

    Updated sorting algo explanations

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -21,9 +21,11 @@ def bubblesort_optimized(arr):
     return arr
 
 def insertionsort(arr):
+    # Assume first element is sorted 
     for i in range(1, len(arr)):
         key = arr[i]
         j = i-1
+        # Move elements within arr[0..i-1] that are greater than key to the right
         while j >= 0 and key < arr[j]:
             arr[j+1] = arr[j]
             j -= 1
