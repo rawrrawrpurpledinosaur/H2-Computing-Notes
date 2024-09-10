a1e0e6bd1103f309a602709e334f9f8995631b9a
rawrrawrpurpledinosaur <191292l@student.hci.edu.sg>
2024-09-10 18:45:29 +0800

    improved binary-denary conversions and vice versa
    made it more readable and added docstrings and explanation

--- conversion_bin_den_hex.py
+++ conversion_bin_den_hex.py
@@ -1,20 +1,22 @@
-def denary_to_binary(n):
+def denary_to_binary(n: int) -> str: # This notation means that function accepts int and returns str
+    """Converts from base 10 to base 2"""
     if n == 0:
         return "0"
 
     binary = ""
     while n>0: 
-        binary = str(n % 2) + binary
-        # same as floor division
-        n = n >> 1
+        binary = str(n % 2) + binary # adds the result of n modulo 2 to the left of result
+        n = n // 2 # floor division
     return binary
 
 def binary_to_denary(n: str) -> int:
+    """Converts from base 2 to base 10"""
+    # Reverse the string to perform conversion more easily
+    n = n[::-1]
     den = 0
-    for i in range(len(n)): 
-        # 1010
-        # 1*2^3 + 0*2^2 + 1*2^1 + 0
-        den = den + int(n[i]) * 2**(len(n)-1-i)
+    for i in range(0, len(n)-1): 
+        # 2^i to convert the i_th digit and add to result
+        den = den + int(n[i]) * 2**i
     return den
 
 
