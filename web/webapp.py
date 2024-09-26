51f244761dd4eb43e47aef2fc8d6a44195eec128
rawrrawrpurpledinosaur <191292l@student.hci.edu.sg>
2024-09-26 15:51:09 +0800

    auto linting

--- web/webapp.py
+++ web/webapp.py
@@ -2,60 +2,73 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect('students.db', check_same_thread=False)
+conn = sqlite3.connect("students.db", check_same_thread=False)
 c = conn.cursor()
 
-# creating tables 
+# creating tables
 # c.execute("""
-# CREATE TABLE IF NOT EXISTS Student (StudentID INTEGER, Name TEXT, Gender TEXT, PRIMARY KEY (StudentID)) 
+# CREATE TABLE IF NOT EXISTS Student (StudentID INTEGER, Name TEXT, Gender TEXT, PRIMARY KEY (StudentID))
 # """)
 #
 # c.execute("""
 # CREATE TABLE IF NOT EXISTS StudentHealthRecord (StudentID INTEGER, Weight INTEGER, Height INTEGER, FOREIGN KEY (StudentID) REFERENCES Student(StudentID))
 # """)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/records')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/records")
 def records():
-    rec = c.execute("""
+    rec = c.execute(
+        """
                     SELECT Name, Gender, Weight, Height
                     FROM Student JOIN StudentHealthRecord
                     ON Student.StudentID = StudentHealthRecord.StudentID
                     ORDER BY Gender ASC, Name DESC 
-                    """).fetchall()
+                    """
+    ).fetchall()
     print(rec)
-    return render_template('records.html', records=rec)
+    return render_template("records.html", records=rec)
 
-@app.route('/stats')
+
+@app.route("/stats")
 def stats():
-    num = c.execute("""
+    num = c.execute(
+        """
                     SELECT SUM(Gender = 'M' OR Gender = 'F'), AVG(Weight), AVG(Height) 
                     FROM Student JOIN StudentHealthRecord
                     ON Student.StudentID = StudentHealthRecord.StudentID
-    """).fetchall()
+    """
+    ).fetchall()
     print(num)
-    return render_template('stats.html', stats = num[0])
+    return render_template("stats.html", stats=num[0])
 
-@app.route('/add', methods=['GET','POST'])
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == 'POST':
-        name = request.form['name']
-        gender = request.form['gender']
-        id = request.form['id']
-        weight = request.form['weight']
-        height = request.form['height']
-        c.execute("""
+    if request.method == "POST":
+        name = request.form["name"]
+        gender = request.form["gender"]
+        id = request.form["id"]
+        weight = request.form["weight"]
+        height = request.form["height"]
+        c.execute(
+            """
         INSERT INTO Student (StudentID, Name, Gender) VALUES (?, ?, ?)
-                """, (id, name, gender))
-        c.execute("""
-        INSERT INTO StudentHealthRecord (StudentID, Weight, Height) VALUES (?, ?, ?)
-                """, (id, weight, height))
+                """,
+            (id, name, gender),
+        )
+        c.execute(
+            """INSERT INTO StudentHealthRecord (StudentID, Weight, Height) VALUES (?, ?, ?)""",
+            (id, weight, height),
+        )
         conn.commit()
         conn.close()
-        return redirect('/records')
-    return render_template('add.html')
+        return redirect("/records")
+    return render_template("add.html")
+
 
 app.run(debug=True)
